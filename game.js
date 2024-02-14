24228d4c357bbc3e93f07abbe6d5a0a9b3e26cde
nekokaito <siddiqsazzad001@gmail.com>
2024-02-14 17:02:06 +0600

    added id

--- game.js
+++ game.js
@@ -3,4 +3,23 @@ function play() {
     home.classList.add('hidden');
     const play = document.getElementById('play');
     play.classList.remove('hidden');
+    continueGame();
+}
+
+function continueGame() {
+    const randomAlpha = getRandom();
+    const displayAlpha = document.getElementById('display');
+    displayAlpha.innerText = randomAlpha;
+     
+}
+function getRandom() {
+    const alphaString = 'abcdefghijklmnopqrstuvwxyz';
+    const alpha = alphaString.slice('');
+    const random = Math.random()*25;
+    const i = Math.round(random);
+    
+    let result = alpha[i];
+
+    return result;
+    
 }

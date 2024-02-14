bf8087ab352aba81774bb757fee57f8650f282e0
nekokaito <siddiqsazzad001@gmail.com>
2024-02-14 14:25:31 +0600

    added play function

---
+++ game.js
@@ -0,0 +1,6 @@
+function play() {
+    const home = document.getElementById('home-screen');
+    home.classList.add('hidden');
+    const play = document.getElementById('play');
+    play.classList.remove('hidden');
+}
